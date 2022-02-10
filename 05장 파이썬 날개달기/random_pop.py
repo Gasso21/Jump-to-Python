676117f7045f5f58b93bc7bd35bedbb4d22dad53
Jaehwan <jhjung1227@naver.com>
2022-02-10 23:49:24 +0900

    '22.02.10.

--- 05장 파이썬 날개달기/random_pop.py
+++ 05장 파이썬 날개달기/random_pop.py
@@ -1,7 +1,6 @@
 # random_pop.py
 
 import random
-"""
 def random_pop(data):
     number = random.randint(0, len(data)-1)
     return data.pop(number)
@@ -10,6 +9,8 @@ if __name__ == "__main__":
     data = [1, 2, 3, 4, 5]
     while data: 
         print(random_pop(data))
+
 """
 data = ['a', 'b', 3, 4, 5]
 print(data.pop(0), (data.pop(0)), data)
+"""

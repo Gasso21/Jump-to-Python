30e2673df9cc09306dfbf330d6859a6941ab1ca8
Jaehwan <jhjung1227@naver.com>
2022-02-13 00:49:03 +0900

    '22.02.12.

--- 연습문제/02장 연습문제.py
+++ 연습문제/02장 연습문제.py
@@ -3,7 +3,7 @@ dic = {'국어': 80, '영어': 75, '수학':55}
 result = 0
 for i in dic.values():
     result += i
-print(result)
+print(result/len(list(dic.keys())))
 
 #Q2
 n = int(input())
@@ -26,6 +26,7 @@ print(a.replace(':','#'))
 
 #Q6
 l = [1, 3, 5, 4, 2]
+l.sort()
 l.reverse()
 print(l)
 
@@ -37,6 +38,11 @@ print(result)
 #Q8
 t = (1,2,3)
 print(t+(4,))
+
+#09
+"""
+ 딕셔너리의 키로는 변하는(mutable) 값을 사용할 수 없음
+"""
 
 #Q10
 a = {'A':90, 'B':80, 'C':70}

701219293a404c4e826b54b0a3e052b79596e228
Jaehwan <jhjung1227@naver.com>
2022-02-13 00:16:49 +0900

    '22.02.12.

---
+++ 연습문제/02장 연습문제.py
@@ -0,0 +1,58 @@
+#Q1
+dic = {'국어': 80, '영어': 75, '수학':55}
+result = 0
+for i in dic.values():
+    result += i
+print(result)
+
+#Q2
+n = int(input())
+if n % 2 == 0:
+    print('짝수')
+else:
+    print('홀수')
+
+#Q3
+pin = "881120-1068234"
+print(pin[:6], pin[7:])
+
+#Q4
+pin = "881120-1068234"
+print(pin[7])
+
+#Q5
+a = "a:b:c:d"
+print(a.replace(':','#'))
+
+#Q6
+l = [1, 3, 5, 4, 2]
+l.reverse()
+print(l)
+
+#Q7
+l = ['Life', 'is', 'too', 'short']
+result = ' '.join(l)
+print(result)
+
+#Q8
+t = (1,2,3)
+print(t+(4,))
+
+#Q10
+a = {'A':90, 'B':80, 'C':70}
+print(a.pop('B'))
+print(a)
+
+#Q11
+a = [1, 1, 1, 2, 2, 3, 3, 3, 4, 4, 5]
+print(list(set(a)))
+
+#Q12
+a = b = [1, 2, 3]
+a[1] = 4
+print(b)
+
+a = [1, 2, 3]
+b = a[:]
+a[1] = 4
+print(b)

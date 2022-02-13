3829cc3aa408590f930ca858f92a9ecf17fdec2a
Jaehwan <jhjung1227@naver.com>
2022-02-13 19:30:41 +0900

    '22.02.13.

---
+++ 연습문제/05장 연습문제.py
@@ -0,0 +1,82 @@
+#Q1
+class Calculator:
+    def __init__(self):
+        self.value = 0
+
+    def add(self, val):
+        self.value += val
+
+class UpgradeCalculator(Calculator):
+    def minus(self, val):
+        self.value -= val
+
+cal = UpgradeCalculator()
+cal.add(10)
+cal.minus(7)
+
+print(cal.value) # 10에서 7을 뺀 3을 출력
+
+#Q2
+class Calculator:
+    def __init__(self):
+        self.value = 0
+
+    def add(self, val):
+        self.value += val
+
+class MaxLimitCalculator(Calculator):
+    def add(self, val):
+        self.value += val
+        if self.value >= 100:
+            self.value = 100
+
+cal = MaxLimitCalculator()
+cal.add(50) # 50 더하기
+cal.add(60) # 60 더하기
+print(cal.value) # 100 출력
+
+#Q3
+print(all([1, 2, abs(-3)-3]))
+"""
+False
+"""
+print(chr(ord('a')) == 'a')
+"""
+True
+"""
+
+#Q4
+l = [1, -2, 3, -5, 8, -3]
+print(list(filter(lambda x: x > 0, l)))
+
+#Q5
+print(hex(234))
+print(int(0xea))
+
+#Q6
+print(list(map(lambda x: x*3, [1, 2, 3, 4])))
+
+#Q7
+mi = 0
+ma = 0
+for i in [-8, 2, 7, 5, -3, 5, 0, 1]:
+    if i < mi:
+        mi = i
+    elif i > ma:
+        ma = i
+
+print(mi+ma)
+
+#Q8
+print(round(17/3, 4))
+
+#Q9
+import sys
+x = sys.argv
+s = 0
+for i in x[1:]:
+    s += int(i)
+print(s)
+
+#Q10
+import os
